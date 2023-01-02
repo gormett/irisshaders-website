eda20a1353977a45198dbe64909f9b775b1e334d
gormett <matus.kobylan@gmail.com>
2023-01-02 04:43:30 +0100

    New icon component. Footer socials

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,27 @@
 <script>
-    let IrisLogo = './svg/iris_logo_white.svg';
+	import Icon from "./Icon.svelte";
+
+    let IrisLogo = './svg/iris_logotype_color.svg';
 </script>
 
-<footer class="bg-black text-zinc-500 mx-auto p-4 space-y-3 flex flex-col">
-    <img src={IrisLogo} class="h-16" alt="Iris Logo" />
-    <h5 class="text-xs text-center">WIP Gormett</h5>
+<footer class="bg-black text-neutral-500 mx-auto p-4 space-y-6 flex flex-col">
     <!-- Logo -->
+    <img src={IrisLogo} class="h-12" alt="Iris Logo" />
 
+    <!-- Socials -->
+    <div class="mx-auto">
+        <div class="flex flex-row h-8 fill-neutral-600 gap-x-10">
+            <Icon name="Youtube" />
+            <Icon name="Github" />
+            <Icon name="Discord" />
+            <Icon name="Twitter" />
+            <Icon name="Mastodon" />
+            <Icon name="Patreon" />
+        </div>
+    </div>
+
+    
+    <h5 class="text-xs text-center">WIP Gormett ©2023</h5>
 </footer>
+
+
